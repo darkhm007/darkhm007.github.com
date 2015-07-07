9d6c19b4b2c39ce712fce954a6a01628084b102f
darkhm007 <darkhm007@live.com>
2015-07-07 18:57:26 +0800

    candy

---
+++ candy/src/Constant.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2015/7/6.
+ */
+var Constant = {
+    CANDY_WIDTH: 64,
+    CANDY_TYPE_COUNT: 5,
+    MAP_SIZE: 10,
+
+    levels:[
+        {limitStep:30, targetScore:500},
+        {limitStep:25, targetScore:1000},
+        {limitStep:20, targetScore:1500},
+        {limitStep:20, targetScore:2000},
+        {limitStep:15, targetScore:3000},
+        {limitStep:10, targetScore:4000}
+    ]
+};
---
+++ candy/src/resource.js
@@ -0,0 +1,17 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    CloseNormal_png : "res/CloseNormal.png",
+    CloseSelected_png : "res/CloseSelected.png"
+};
+
+var g_resources = [
+    "res/1.png",
+    "res/2.png",
+    "res/3.png",
+    "res/5.png",
+    "res/5.png",
+    "res/bg.jpg"
+];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
